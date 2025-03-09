155f60100180ac3bc97e0862e3dc1c06e733e2d9
bruins001 <niels.bruinsma2002@outlook.com>
2025-03-09 21:09:02 +0100

    Update api.php change the book POST route to books

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\LibraryController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('book', [LibraryController::class, 'addBook']);
+Route::post('books', [LibraryController::class, 'addBook']);
 Route::post('member', [LibraryController::class, 'addMember']);
 Route::get('books/available', [LibraryController::class, 'listAvailableBooks']);
 Route::get('books/borrowed', [LibraryController::class, 'listBorrowedBooks']);
