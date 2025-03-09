5f2ef1a86bf6350eea965cfeb6414ab5067088f3
bruins001 <niels.bruinsma2002@outlook.com>
2025-03-09 20:38:00 +0100

    Add add member functionality

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Models\Member;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -35,5 +36,23 @@ class LibraryController extends Controller
         $book->save();
 
         return response()->json(['status' => 'success', 'data' => $book], 201);
+    }
+
+    function addMember(Request $request): JsonResponse {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|unique:members,name',
+            'email' => 'required|string|email|unique:members,email',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json(['status' => 'error', 'error' => $validator->errors()], 400);
+        }
+
+        $member = new Member();
+        $member->name = strtolower($request->input('name'));
+        $member->email = strtolower($request->input('email'));
+        $member->save();
+
+        return response()->json(['status' => 'success', 'data' => $member], 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\LibraryController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('book', [LibraryController::class, 'addBook']);
+Route::post('member', [LibraryController::class, 'addMember']);
